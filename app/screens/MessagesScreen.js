f449ecf39243a70d7162a060cf174b85be8b7fe0
jonanpogi <junan.bie@gmail.com>
2020-12-20 15:37:48 +0800

    Created Message Screen for messages

---
+++ app/screens/MessagesScreen.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { FlatList, StyleSheet } from "react-native";
+import AppListItem from "../components/AppListItem";
+import AppBlankScreen from "../components/AppBlankScreen";
+import AppListSeparator from "../components/AppListSeparator";
+import AppListItemDeleteAction from "../components/AppListItemDeleteAction";
+
+const messages = [
+    {
+        id: 1,
+        title: "T1",
+        decription: "D1",
+        image: require("../assets/mosh.jpg"),
+    },
+    {
+        id: 2,
+        title: "T2",
+        decription: "D2",
+        image: require("../assets/mosh.jpg"),
+    },
+    {
+        id: 3,
+        title: "T3",
+        decription: "D3",
+        image: require("../assets/mosh.jpg"),
+    },
+];
+
+function MessagesScreen(props) {
+    const [data, setData] = useState(messages);
+    const [refreshing, setRefreshing] = useState(false);
+
+    const handleDeleteItem = (id) => {
+        setData(data.filter((obj) => obj.id !== id));
+    };
+
+    const handleOnRefresh = () => {
+        setData(messages);
+    };
+
+    return (
+        <AppBlankScreen>
+            <FlatList
+                data={data}
+                keyExtractor={(data) => data.id.toString()}
+                renderItem={({ item }) => (
+                    <AppListItem
+                        title={item.title}
+                        subTitle={item.decription}
+                        image={item.image}
+                        onPress={() => alert("TEST")}
+                        renderRightActions={() => (
+                            <AppListItemDeleteAction
+                                onPress={() => handleDeleteItem(item.id)}
+                            />
+                        )}
+                    />
+                )}
+                ItemSeparatorComponent={() => <AppListSeparator />}
+                refreshing={refreshing}
+                onRefresh={handleOnRefresh}
+            />
+        </AppBlankScreen>
+    );
+}
+
+const styles = StyleSheet.create({});
+
+export default MessagesScreen;
