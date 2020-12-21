157e5b76ee9a6905f433ca1193f961c7b1bf3174
jonanpogi <junan.bie@gmail.com>
2020-12-21 18:11:55 +0800

    Added new props which are the numColumns for Item List Columns, PickerItemComponent for AppCategoryItemPicker.js, and width for setting the width of AppFormPicker.js

--- app/components/forms/AppFormPicker.js
+++ app/components/forms/AppFormPicker.js
@@ -4,18 +4,28 @@ import { useFormikContext } from "formik";
 import AppPicker from "../AppPicker";
 import AppErrorMessage from "./AppErrorMessage";
 
-function AppFormPicker({ name, items, placeholder }) {
+function AppFormPicker({
+    name,
+    numColumns,
+    items,
+    PickerItemComponent,
+    placeholder,
+    width,
+}) {
     const { setFieldValue, errors, touched, values } = useFormikContext();
 
     return (
         <>
             <AppPicker
                 items={items}
+                numColumns={numColumns}
                 onSelectedItem={(selectedItem) =>
                     setFieldValue(name, selectedItem)
                 }
+                PickerItemComponent={PickerItemComponent}
                 placeholder={placeholder}
                 selectedItem={values[name]}
+                width={width}
             />
             <AppErrorMessage error={errors[name]} visible={touched[name]} />
         </>
