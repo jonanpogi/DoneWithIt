f2f92e810fdec2ceff7f2275037327f5add9615d
jonanpogi <junan.bie@gmail.com>
2020-12-21 18:13:05 +0800

    Added Width props

--- app/components/AppTextInput.js
+++ app/components/AppTextInput.js
@@ -5,9 +5,9 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import defaultStyles from "../config/styles";
 import colors from "../config/colors";
 
-function AppTextInput({ icon, ...otherProps }) {
+function AppTextInput({ icon, width = "100%", ...otherProps }) {
     return (
-        <View style={styles.container}>
+        <View style={[styles.container, { width }]}>
             {icon && (
                 <MaterialCommunityIcons
                     name={icon}
@@ -30,7 +30,6 @@ const styles = StyleSheet.create({
         backgroundColor: defaultStyles.colors.light,
         borderRadius: 25,
         flexDirection: "row",
-        width: "100%",
         padding: 15,
         marginVertical: 10,
         justifyContent: "flex-start",
