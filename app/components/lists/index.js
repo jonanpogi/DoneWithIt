d654e293f162610cdc8c5e530f9d69e53c4cd259
jonanpogi <junan.bie@gmail.com>
2020-12-21 13:03:29 +0800

    Added index for lists folder

---
+++ app/components/lists/index.js
@@ -0,0 +1,3 @@
+export { default as AppListItem } from "./AppListItem";
+export { default as AppListItemDeleteAction } from "./AppListItemDeleteAction";
+export { default as AppListSeparator } from "./AppListSeparator";
