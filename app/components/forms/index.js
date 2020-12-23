0bca925dc1e47115becc1e3be83a87fb1c553501
jonanpogi <junan.bie@gmail.com>
2020-12-23 12:44:24 +0800

    Updated index.js

--- app/components/forms/index.js
+++ app/components/forms/index.js
@@ -3,3 +3,4 @@ export { default as AppFormField } from "./AppFormField";
 export { default as AppSubmitButton } from "./AppSubmitButton";
 export { default as AppErrorMessage } from "./AppErrorMessage";
 export { default as AppFormPicker } from "./AppFormPicker";
+export { default as AppFormImagePicker } from "./AppFormImagePicker";
