fb7cb6513ea3a4f84682e211504d06bcf29d408e
jonanpogi <junan.bie@gmail.com>
2020-12-24 10:08:19 +0800

    Created route constants

---
+++ app/navigation/routes.js
@@ -0,0 +1,7 @@
+export default Object.freeze({
+    LISTING_EDIT_SCREEN: "ListingEditScreen",
+    LISTING_DETAILS_SCREEN: "ListingDetailsScreen",
+    MESSAGES_SCREEN: "MessagesScreen",
+    LOGIN_SCREEN: "LoginScreen",
+    REGISTER_SCREEN: "RegisterScreen",
+});
