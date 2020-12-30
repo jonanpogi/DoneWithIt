a24ff9bbfd55dcf76d256ba95a04cb1f90cf82bf
jonanpogi <junan.bie@gmail.com>
2020-12-30 11:15:07 +0800

    added postListings api call

--- app/api/listings.js
+++ app/api/listings.js
@@ -4,6 +4,35 @@ const endpoint = "/listings";
 
 const getListings = () => client.get(endpoint);
 
+const postListings = (values, onUploadProgress) => {
+    const form = { ...values };
+    const data = new FormData();
+
+    for (let key of Object.keys(form)) {
+        if (key === "images") {
+            form[key].forEach((file, index) => {
+                data.append(key, {
+                    name: "image" + index,
+                    type: "image/jpeg",
+                    uri: file,
+                });
+            });
+        } else if (key === "location") {
+            form[key] !== null && data.append(key, JSON.stringify(form[key]));
+        } else if (key === "category") {
+            data.append("categoryId", form[key].value);
+        } else {
+            data.append(key, form[key]);
+        }
+    }
+
+    return client.post(endpoint, data, {
+        onUploadProgress: ({ loaded, total }) =>
+            onUploadProgress(loaded / total),
+    });
+};
+
 export default {
     getListings,
+    postListings,
 };
