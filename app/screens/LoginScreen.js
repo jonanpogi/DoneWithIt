9044be78786c54f6febc4f5ba4fe2c7615ae63e3
jonanpogi <junan.bie@gmail.com>
2021-01-08 14:19:43 +0800

    Updated RegisterScreen and LoginScreen to make it functional for authentication flow.

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Image } from "react-native";
 import colors from "../config/colors";
 import * as Yup from "yup";
 
 import AppBlankScreen from "../components/AppBlankScreen";
-import { AppForm, AppFormField, AppSubmitButton } from "../components/forms";
+import {
+    AppForm,
+    AppFormField,
+    AppSubmitButton,
+    AppErrorMessage,
+} from "../components/forms";
+import auth from "../api/auth";
+import useAuth from "../auth/useAuth";
+import useApi from "../hooks/useApi";
+import AppActivityIndicator from "../components/AppActivityIndicator";
 
 const validationSchema = Yup.object().shape({
     email: Yup.string().required().email().label("Email"),
@@ -12,40 +21,64 @@ const validationSchema = Yup.object().shape({
 });
 
 function LoginScreen(props) {
+    const loginApi = useApi(auth.login);
+    const { logIn } = useAuth();
+    const [visible, setVisible] = useState(false);
+
+    const handleSubmit = async ({ email, password }) => {
+        const result = await loginApi.request(email, password);
+
+        if (!result.ok) {
+            return setVisible(true);
+        } else {
+            setVisible(false);
+            logIn(result.data);
+        }
+    };
+
     return (
-        <AppBlankScreen style={styles.container}>
-            <Image
-                style={styles.logo}
-                source={require(`../assets/logo-red.png`)}
-            />
-            <AppForm
-                initialValues={{ email: "", password: "" }}
-                onSubmit={(values) => console.log(values)}
-                validationSchema={validationSchema}
-            >
-                <AppFormField
-                    autoCaptilize="none"
-                    autoCorrect={false}
-                    icon="email"
-                    keyboardType="email-address"
-                    name="email"
-                    placeholder="Email"
-                    textContentType="emailAddress"
+        <>
+            <AppActivityIndicator visible={loginApi.loading} />
+            <AppBlankScreen style={styles.container}>
+                <Image
+                    style={styles.logo}
+                    source={require(`../assets/logo-red.png`)}
                 />
 
-                <AppFormField
-                    autoCaptilize="none"
-                    autoCorrect={false}
-                    icon="lock"
-                    name="password"
-                    placeholder="Password"
-                    secureTextEntry
-                    textContentType="password"
+                <AppErrorMessage
+                    error="Invalid email and/or password."
+                    visible={visible}
                 />
 
-                <AppSubmitButton title="submit" color={colors.primary} />
-            </AppForm>
-        </AppBlankScreen>
+                <AppForm
+                    initialValues={{ email: "", password: "" }}
+                    onSubmit={handleSubmit}
+                    validationSchema={validationSchema}
+                >
+                    <AppFormField
+                        autoCaptilize="none"
+                        autoCorrect={false}
+                        icon="email"
+                        keyboardType="email-address"
+                        name="email"
+                        placeholder="Email"
+                        textContentType="emailAddress"
+                    />
+
+                    <AppFormField
+                        autoCaptilize="none"
+                        autoCorrect={false}
+                        icon="lock"
+                        name="password"
+                        placeholder="Password"
+                        secureTextEntry
+                        textContentType="password"
+                    />
+
+                    <AppSubmitButton title="submit" color={colors.primary} />
+                </AppForm>
+            </AppBlankScreen>
+        </>
     );
 }
 
--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 import AppBlankScreen from "../components/AppBlankScreen";
-import { AppForm, AppFormField, AppSubmitButton } from "../components/forms";
+import {
+    AppErrorMessage,
+    AppForm,
+    AppFormField,
+    AppSubmitButton,
+} from "../components/forms";
 import colors from "../config/colors";
 import * as Yup from "yup";
+import auth from "../api/auth";
+import useAuth from "../auth/useAuth";
+import useApi from "../hooks/useApi";
+import AppActivityIndicator from "../components/AppActivityIndicator";
 
 const validationSchema = Yup.object().shape({
     name: Yup.string().required().label("Name"),
@@ -12,50 +21,83 @@ const validationSchema = Yup.object().shape({
 });
 
 function RegisterScreen(props) {
+    const registerApi = useApi(auth.register);
+    const loginApi = useApi(auth.login);
+    const { logIn } = useAuth();
+    const [error, setError] = useState(null);
+
+    const handleSubmit = async (userInfo) => {
+        const result = await registerApi.request(userInfo);
+
+        if (!result.ok) {
+            if (result.data) setError(result.data.error);
+            else {
+                setError("An unexpected error occured.");
+                console.log(result);
+            }
+            return;
+        }
+
+        const { data: authToken } = await loginApi.request(
+            userInfo.email,
+            userInfo.password
+        );
+        logIn(authToken);
+    };
+
     return (
-        <AppBlankScreen style={styles.container}>
-            <AppForm
-                initialValues={{ name: "", email: "", password: "" }}
-                onSubmit={(values) => console.log(values)}
-                validationSchema={validationSchema}
-            >
-                <AppFormField
-                    autoCaptilize="none"
-                    autoCorrect={false}
-                    icon="account"
-                    name="name"
-                    placeholder="Name"
-                    textContentType="name"
-                />
+        <>
+            <AppActivityIndicator
+                visible={registerApi.loading || loginApi.loading}
+            />
+            <AppBlankScreen style={styles.container}>
+                <AppForm
+                    initialValues={{ name: "", email: "", password: "" }}
+                    onSubmit={handleSubmit}
+                    validationSchema={validationSchema}
+                >
+                    <AppErrorMessage error={error} visible={error !== null} />
 
-                <AppFormField
-                    autoCaptilize="none"
-                    autoCorrect={false}
-                    icon="email"
-                    keyboardType="email-address"
-                    name="email"
-                    placeholder="Email"
-                    textContentType="emailAddress"
-                />
+                    <AppFormField
+                        autoCaptilize="none"
+                        autoCorrect={false}
+                        icon="account"
+                        name="name"
+                        placeholder="Name"
+                        textContentType="name"
+                    />
 
-                <AppFormField
-                    autoCaptilize="none"
-                    autoCorrect={false}
-                    icon="lock"
-                    name="password"
-                    placeholder="Password"
-                    secureTextEntry
-                    textContentType="password"
-                />
+                    <AppFormField
+                        autoCaptilize="none"
+                        autoCorrect={false}
+                        icon="email"
+                        keyboardType="email-address"
+                        name="email"
+                        placeholder="Email"
+                        textContentType="emailAddress"
+                    />
 
-                <AppSubmitButton title="register" color={colors.primary} />
-            </AppForm>
-        </AppBlankScreen>
+                    <AppFormField
+                        autoCaptilize="none"
+                        autoCorrect={false}
+                        icon="lock"
+                        name="password"
+                        placeholder="Password"
+                        secureTextEntry
+                        textContentType="password"
+                    />
+
+                    <AppSubmitButton title="register" color={colors.primary} />
+                </AppForm>
+            </AppBlankScreen>
+        </>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {},
+    container: {
+        padding: 10,
+    },
 });
 
 export default RegisterScreen;
